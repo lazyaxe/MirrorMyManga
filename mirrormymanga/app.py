0512a564f2d0f83d84e4a3948fd79e99b27b66d5
Harsh Sharma <itisharshsharma18@gmail.com>
2026-07-27 23:25:14 +0530

    Revert "renamed app.py to __main__.py"
    This reverts commit 518d48d3d84a55f2d1e8d0520cc4944d8bc11ceb.

--- mirrormymanga/app.py
+++ mirrormymanga/app.py
@@ -1,9 +1,10 @@
-from .ocr import create_ocr
 from .service import transform_pdf, transform_cbz, CLI
+from .ocr import create_ocr
+from .utils import is_cuda_available
 import time
 import os
 from pathlib import Path
-from .settings import OCRSettings, TransformPDFSettings, TransformCBZSettings
+from .settings import OCRSettings, TransformPanelSettings, TransformPDFSettings, TransformCBZSettings
 
 if __name__ == "__main__":
     start = time.perf_counter()
@@ -12,7 +13,7 @@ if __name__ == "__main__":
     #USAGE EXAMPLE FOR A SCRIPT:
     #input_path = "/home/vhvhs/Test/testPDF4.pdf"
     #output_path = os.path.join(Path.cwd(), "testPDF4_result.cbz")
-    #ocrSettings = OCRSettings()
+    #ocrSettings = OCRSettings(lang="")
     #ocr = create_ocr(ocrSettings=OCRSettings(lang="en"))
     #transform_pdf(ocr=ocr, input_path=input_path, output_path=output_path, transformPDFSettings=TransformPDFSettings(dpi=100))
-    print(f"Program ended in {time.perf_counter() - start}s")
+    #print(f"Program ended in {time.perf_counter() - start}s")
